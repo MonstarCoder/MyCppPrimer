003caca23d97700d7beef721a3a14724b456ec13
Monstar <652141046@qq.com>
2017-03-26 17:07:41 +0800

    ch17. on 17/3/26

---
+++ ch17/ex17_1_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <tuple>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::tuple<int, int, int> i3{12, 20, 30};
+    std::tuple<std::string, std::vector<std::string>, std::pair<std::string, int>> t;
+    return 0;
+}
