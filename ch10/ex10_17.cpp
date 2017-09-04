8d50bbdff719ed62c22630654b0630c779dcc943
Monstar <652141046@qq.com>
2017-09-04 14:25:07 +0800

    update ch10

---
+++ ch10/ex10_17.cpp
@@ -0,0 +1,21 @@
+#include "../ch07/ex07_26.h" // Sales_data class.
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    Sales_data d1("aa"), d2("aaaa"), d3("aaa"), d4("z"), d5("aaaaz");
+    std::vector<Sales_data> v{d1, d2, d3, d4, d5};
+
+    std::sort(v.begin(), v.end(),
+              [](const Sales_data& sd1, const Sales_data& sd2) {
+                  return sd1.isbn().size() < sd2.isbn().size();
+              });
+
+    for (const auto& element : v) std::cout << element.isbn() << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
