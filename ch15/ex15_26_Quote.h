3ec5c19a8f80010b78f86eaee2341985fa879f6c
Monstar <652141046@qq.com>
2017-09-25 17:05:48 +0800

    update ch15 on 2017.9.25

--- ch15/ex15_26_Quote.h
+++ ch15/ex15_26_Quote.h
@@ -5,52 +5,52 @@
 #include <iostream>
 
 namespace EX26 {
-	    using std::string;
-	        using std::cout; using std::endl;
+    using std::string;
+    using std::cout; using std::endl;
 
-		class Quote {
-			public:
-				    Quote() {
-					            cout << "Quote Constructor" << endl;
-						        }
+class Quote {
+public:
+    Quote() {
+        cout << "Quote Constructor" << endl;
+    }
 
-				        Quote(const string &b, double p) : bookNo(b), price(p) {
-						        cout << "Quote Constructor taking two parameters" << endl;
-							    }
+    Quote(const string &b, double p) : bookNo(b), price(p) {
+        cout << "Quote Constructor taking two parameters" << endl;
+    }
 
-					    Quote(const Quote &rhs) : bookNo(rhs.bookNo), price(rhs.price) {
-						            cout << "Quote Copy Constructor" << endl;
-							        }
+    Quote(const Quote &rhs) : bookNo(rhs.bookNo), price(rhs.price) {
+        cout << "Quote Copy Constructor" << endl;
+    }
 
-					        Quote& operator=(const Quote &rhs) {
-							        cout << "Quote Copy assignment operator" << endl;
-								        price = rhs.price;
-									        bookNo = rhs.bookNo;
-										        return *this;
-											    }
+    Quote& operator=(const Quote &rhs) {
+        cout << "Quote Copy assignment operator" << endl;
+        price = rhs.price;
+        bookNo = rhs.bookNo;
+        return *this;
+    }
 
-						    Quote(Quote &&rhs) noexcept : bookNo(std::move(rhs.bookNo)), price(std::move(rhs.price)) {
-							            cout << "Quote Move Constructor" << endl;
-								        }
+    Quote(Quote &&rhs) noexcept : bookNo(std::move(rhs.bookNo)), price(std::move(rhs.price)) {
+        cout << "Quote Move Constructor" << endl;
+    }
 
-						        Quote& operator=(Quote &&rhs) noexcept {
-								        cout << "Quote Move assignment operator" << endl;
-									        bookNo = std::move(rhs.bookNo);
-										        price = std::move(rhs.price);
-											        return *this;
-												    }
+    Quote& operator=(Quote &&rhs) noexcept {
+        cout << "Quote Move assignment operator" << endl;
+        bookNo = std::move(rhs.bookNo);
+        price = std::move(rhs.price);
+        return *this;
+    }
 
-							    virtual ~Quote() {
-								            cout << "Quote Destructor" << endl;
-									        }
+    virtual ~Quote() {
+        cout << "Quote Destructor" << endl;
+    }
 
-							        string isbn() const { return bookNo; }
-								    virtual double net_price(size_t n) const { return n * price; }
-			private:
-								        string bookNo;
-			protected:
-									    double price = 0.0;
-		};
+    string isbn() const { return bookNo; }
+    virtual double net_price(size_t n) const { return n * price; }
+private:
+    string bookNo;
+protected:
+    double price = 0.0;
+};
 
 }
 
--- ch15/ex15_27_Bulk_quote.h
+++ ch15/ex15_27_Bulk_quote.h
@@ -1,20 +1,22 @@
-#ifndef CP5_27_BULK_QUOTE_H_
-#define CP5_27_BULK_QUOTE_H_
+
+#ifndef CP5_EX15_27_BULK_QUOTE_H
+#define CP5_EX15_27_BULK_QUOTE_H
 
 #include "ex15_15_Disc_quote.h"
 
 inline namespace EX27 {
-	using std::string;
+
+    using std::string;
 
 class Bulk_quote : public Disc_quote {
 public:
-	using Disc_quote::Disc_quote;
-	virtual double net_price(std::size_t cnt) const override {
-		if ( cnt >= quantity) return cnt * (1 - discount) * price;
-		esle
-			return cnt * price;
-	}
+    using Disc_quote::Disc_quote;
+    virtual double net_price(std::size_t cnt) const override {
+        if (cnt >= quantity) return cnt * (1 - discount) * price;
+        else return cnt * price;
+    }
 };
+
 }
 
-#endif
+#endif // CP5_EX15_27_BULK_QUOTE_H
