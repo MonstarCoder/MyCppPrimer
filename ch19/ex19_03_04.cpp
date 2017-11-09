d57b90dbd102c6a7ad9449ca7d493fa4be23878d
Marvin <652141046@qq.com>
2017-11-09 15:23:20 +0800

    update ch19 on 2017.11.9

---
+++ ch19/ex19_03_04.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+class A {
+public:
+	virtual ~A() {}
+};
+
+class B : public A {
+public:
+	virtual ~B() {}
+};
+
+class C : public B {
+public:
+	virtual ~C() {}
+};
+
+class D : public B, public A {
+public:
+	virtual ~D() {}
+};
+
+int main(int argc, char const *argv[])
+{
+	/* Exercise 19.3 */
+
+	A *pa = new C;    
+	B *pb = dynamic_cast< B* >(pa); 
+	if (pb) cout << "19.3 (a) succeed!" << endl;
+	else cout << "19.3 (a) fail!" << endl;
+
+	pb = new B;    
+	C *pc = dynamic_cast< C* >(pb); 
+	if (pc) cout << "19.3 (b) succeed!" << endl;
+	else cout << "19.3 (b) fail!" << endl;
+
+	pa = new D;    
+	//pb = dynamic_cast< B* >(pa); */
+
+	/* Exercise 19.4 */
+
+	C c; B b;
+	A &ra1 = c, &ra2 = b;
+	try {
+		/* succeed */
+		C &rc1 = dynamic_cast<C&>(ra1);
+		cout << "19.4 succeed!" << endl;
+		/* fail */
+		C &rc2 = dynamic_cast<C&>(ra2);
+	} catch (bad_cast) {
+		cout << "19.4 failed!" << endl;
+	}
+	return 0;
+}
