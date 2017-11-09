205681dc1083519e8581185608ccb6bea14fb4fa
Marvin <652141046@qq.com>
2017-11-09 20:12:35 +0800

    update ch11 on 2017.11.9

---
+++ ch19/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>  
+using namespace std;  
+  
+class Base1  
+{  
+        public:  
+                 void test(){cout<<"base1 test"<<endl;}  
+};  
+  
+class Base2  
+{  
+        public:  
+                void test(){cout<<"base2 test"<<endl;}  
+};  
+  
+  
+class Test:public Base1,public Base2  
+{  
+        public:  
+  
+};  
+  
+int main()  
+{  
+        Base1 * test = new Test;  
+        test->test();  
+}  
